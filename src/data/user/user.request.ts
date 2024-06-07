7811358ed4526bf20d7d41df89d76b6f66fd9bf7
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2024-06-07 16:33:50 -0300

    chore: fixing create user route path

--- src/data/user/user.request.ts
+++ src/data/user/user.request.ts
@@ -2,7 +2,7 @@ import { TUserCreate } from "@/types/user";
 import { localApi } from "../api/local";
 
 const create = async (body: TUserCreate) => {
-   const { data } = await localApi.post("/users", body);
+   const { data } = await localApi.post("/users/create", body);
 
    return data;
 };
